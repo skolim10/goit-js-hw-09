0d05a5d4440dd9f0f112e4c30f46a3e7c77772b2
Tomasz Skolimo <skolim10@gmail.com>
2022-08-12 21:58:39 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let colorChange = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+
+  colorChange = setInterval(() => {
+    const color = getRandomHexColor();
+    body.style.background = color;
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  stopBtn.disabled = true;
+  startBtn.disabled = false;
+  clearInterval(colorChange);
+});
